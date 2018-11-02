e008d310ff1fdfeb79a940aedbf6bfea66a0076c
wwwjf <wwwjf2015@outlook.com>
2018-11-03 02:45:26 +0800

    rxjava map

--- app/src/main/java/com/wwwjf/demo/rxandroid/RxAndroidActivity.java
+++ app/src/main/java/com/wwwjf/demo/rxandroid/RxAndroidActivity.java
@@ -31,6 +31,7 @@ import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class RxAndroidActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener {
@@ -83,10 +84,7 @@ public class RxAndroidActivity extends Activity implements SwipeRefreshLayout.On
             }
         });*/
 
-        getAppInfo()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<AppInfoEntity>() {
+        /*getAppInfo().subscribe(new Observer<AppInfoEntity>() {
             @Override
             public void onSubscribe(Disposable d) {
                 mDataList.clear();
@@ -108,6 +106,59 @@ public class RxAndroidActivity extends Activity implements SwipeRefreshLayout.On
                 mSwipeRefreshLayout.setRefreshing(false);
                 mAppInfoAdapter.notifyDataSetChanged();
                 Toast.makeText(RxAndroidActivity.this, "查询完成,size="+mDataList.size(), Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        getAppInfo2().subscribe(new Observer<List<AppInfoEntity>>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e(TAG, "onSubscribe: =======");
+                mDataList.clear();
+                mSwipeRefreshLayout.setRefreshing(true);
+
+            }
+
+            @Override
+            public void onNext(List<AppInfoEntity> appInfoEntities) {
+                mDataList.addAll(appInfoEntities);
+                Log.e(TAG, "onNext: =======");
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, "onComplete: =======");
+                mSwipeRefreshLayout.setRefreshing(false);
+                mAppInfoAdapter.notifyDataSetChanged();
+                Toast.makeText(RxAndroidActivity.this, "查询完成2，size="+mDataList.size(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mapFunc();
+    }
+
+    private void mapFunc() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(100);
+                emitter.onNext(200);
+                emitter.onNext(300);
+            }
+        }).map(new Function<Integer, String>() {
+            @Override
+            public String apply(Integer integer) throws Exception {
+                return "Integer类型转换为 String类型 "+integer;
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.e(TAG, "accept: s="+s);
             }
         });
     }
@@ -133,7 +184,28 @@ public class RxAndroidActivity extends Activity implements SwipeRefreshLayout.On
                 }
                 emitter.onComplete();
             }
-        });
+        }).subscribeOn(Schedulers.io())//子线程中执行
+                .observeOn(AndroidSchedulers.mainThread());//UI线程中更新
+    }
+    private Observable<List<AppInfoEntity>> getAppInfo2() {
+        return Observable.create(new ObservableOnSubscribe<List<AppInfoEntity>>() {
+            @Override
+            public void subscribe(ObservableEmitter<List<AppInfoEntity>> emitter) throws Exception {
+                List<AppInfoEntity> list = new ArrayList<>();
+                Intent intent = new Intent(Intent.ACTION_MAIN, null);
+                List<PackageInfo> installedList = getApplication().getPackageManager().getInstalledPackages(0);
+                Log.e(TAG, "subscribe: thread  "+Thread.currentThread().getName());
+                for (PackageInfo packageInfo : installedList) {
+                    AppInfoEntity appInfoEntity = new AppInfoEntity();
+                    appInfoEntity.setName(packageInfo.packageName);
+                    appInfoEntity.setIcon(packageInfo.applicationInfo.loadIcon(getApplication().getPackageManager()));
+                    list.add(appInfoEntity);
+                }
+                emitter.onNext(list);
+                emitter.onComplete();
+            }
+        }).subscribeOn(Schedulers.io())//子线程中执行
+                .observeOn(AndroidSchedulers.mainThread());//UI线程中更新
     }
 
     @Override
